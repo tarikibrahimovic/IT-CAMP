c735d0200eabee057c6d60b865f34ad34b39e2b5
Tarik Ibrahimovic <tarikibrahimovic2016@gmail.com>
2023-10-07 18:04:44 +0200

    loops recap

--- javascript/loops/app.js
+++ javascript/loops/app.js
@@ -557,10 +557,62 @@
 
 ////////////////////2.28
 
-let n = parseInt(prompt("Unesite broj:"))
+// let n = parseInt(prompt("Unesite broj:"))
 
-let n500 = Math.floor(n / 500)
-let n100 = Math.floor((n % 500) / 100)
-let n1 =  n % 100
+// let n500 = Math.floor(n / 500)
+// let n100 = Math.floor((n % 500) / 100)
+// let n1 =  n % 100
 
-console.log(n500, n100, n1);
+// console.log(n500, n100, n1);
+
+
+//////////////////////2.29
+
+// let radijani = parseFloat(prompt("Unesite broj:"))
+
+// radijani = radijani * 180 / Math.PI
+
+// let stepeni = Math.floor(radijani)
+
+// radijani = radijani - stepeni
+
+// let minuti = Math.floor(radijani* 60)
+
+// console.log(radijani)
+
+// console.log(stepeni, minuti, sekunda);
+
+// let x = 5
+
+// let y = 4
+
+// console.log(x&y);
+// console.log(x|y);
+// console.log(x>>y);
+// console.log(x<<y);
+
+/////////////////////////////////////////3.13
+
+// let a = parseFloat(prompt("Unesite a"))
+// let b = parseFloat(prompt("Unesite b"))
+// let c = parseFloat(prompt("Unesite c"))
+// let p = 0;
+// let s = 0;
+
+// if(a+b>c || b+c>a || c+a>b){
+//     s = (a+b+c)/2
+//     p = Math.sqrt(s*(s-a)*(s-b)*(s-c))
+// }
+// console.log(p || "ne moze da se napravi trougao");
+
+/////////////////////////////////////4.5
+
+let n = parseFloat(prompt("Unesite zeljeni broj elemenata:"))
+let suma = 0;
+
+for(let i=0; i<n;i++){
+    let broj = parseFloat(prompt(`Unesite ${i+1} element:`))
+    suma += broj
+}
+
+console.log(`Suma je: ${suma} a srednja vrednost je: ${suma/n}`);
