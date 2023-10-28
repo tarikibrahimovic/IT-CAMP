42fd2c49c3ee080b905433b7e59c59f481b64b5e
Tarik Ibrahimovic <tarikibrahimovic2016@gmail.com>
2023-10-28 18:14:45 +0200

    switch case exercise

--- javascript/continue/app.js
+++ javascript/continue/app.js
@@ -119,10 +119,116 @@
 
 //////////////////////////////////////7.24
 
-let godina = parseInt(prompt("Unesite godinu: "));
-let mesec = parseInt(prompt("Unesite redni broj meseca: "));
+// let godina = parseInt(prompt("Unesite godinu: "));
+// let mesec = parseInt(prompt("Unesite redni broj meseca: "));
 
-switch (mesec) {
+// switch (mesec) {
+//   case 1:
+//   case 3:
+//   case 5:
+//   case 7:
+//   case 8:
+//   case 10:
+//   case 12:
+//     console.log("31");
+//     break;
+//   case 4:
+//   case 6:
+//   case 9:
+//   case 11:
+//     console.log(30);
+//   case 2:
+    // (godina % 4 === 0 && godina % 100 !== 0) || godina % 400 === 0
+    //   ? console.log(29)
+    //   : console.log(28);
+      
+//     break;
+//   default:
+//     console.log("Nesto nije dobro");
+//     break;
+// }
+
+//////////////////////////////////////////////
+
+// for(let i=1; i<=10; i++){
+//   console.log("NEKA PORUKA");
+//   if(i === 5) continue;
+//   console.log(i);
+// }
+
+//////////////////////////////////////////7.11
+
+// for(let i=1; i<=20; i++){
+//   if(i % 3 === 0) continue;
+//   console.log(i);
+// }
+
+// let pom1 = 3;
+// let pom2 = "3"
+
+// console.log(pom1 === pom2);
+
+/////////////////////////////////////////////
+
+// let slovo = prompt("Unesite slovo:")
+
+// switch (slovo) {
+//   case "a":
+//   case "e":
+//   case "i":
+//   case "o":
+//   case "u":
+//     console.log("samoglasnik");
+//     break;
+//   default:
+//     console.log("suglasnik");
+//     break;
+// }
+
+///////////////////////////////////////7.24
+
+// let godina = parseInt(prompt("Unesite godinu:"))
+// let mesec = parseInt(prompt("Unesite mesec:"))
+
+// switch (mesec){
+//   case 1:
+//   case 3:
+//   case 5:
+//   case 7:
+//   case 8:
+//   case 10:
+//   case 12:
+//     console.log("31");
+//     break;
+//   case 4:
+//   case 6:
+//   case 9:
+//   case 11:
+//     console.log("30");
+//     break;
+//   case 2:
+//     if((godina % 4 === 0 && godina % 100 !== 0) || (godina % 400 === 0)){
+//       console.log("29");
+//     }
+//     else {
+//       console.log("28");
+//     }
+//     break;
+//   default:
+//     console.log("mesec nije dobar");
+//     break;
+// }
+
+///////////////////////////7.25
+
+
+let godina = parseInt(prompt("Unesite godinu:"))
+let mesec = parseInt(prompt("Unesite mesec:"))
+let dan = parseInt(prompt("Unesite dan:"))
+
+let d = 0;
+
+switch (mesec){
   case 1:
   case 3:
   case 5:
@@ -130,20 +236,43 @@ switch (mesec) {
   case 8:
   case 10:
   case 12:
-    console.log("31");
+    // console.log("31");
+    d = 31;
     break;
   case 4:
   case 6:
   case 9:
   case 11:
-    console.log(30);
+    // console.log("30");
+    d = 30;
+    break;
   case 2:
-    (godina % 4 === 0 && godina % 100 !== 0) || godina % 400 === 0
-      ? console.log(29)
-      : console.log(28);
-      
+    if((godina % 4 === 0 && godina % 100 !== 0) || (godina % 400 === 0)){
+      // console.log("29");
+      d = 29
+    }
+    else {
+      // console.log("28");
+      d=28
+    }
     break;
   default:
-    console.log("Nesto nije dobro");
+    console.log("mesec nije dobar");
     break;
 }
+
+
+if(dan === d){
+  dan = 1;
+  mesec++;
+}
+else{
+  dan++;
+}
+if(mesec > 12){
+  mesec = 1;
+  dan = 1;
+  godina ++;
+}
+
+console.log(dan, mesec, godina);
