de8102e6d69bd69c7a868750ed9f13a6d42a9461
Tarik Ibrahimovic <tarikibrahimovic2016@gmail.com>
2023-11-11 20:11:36 +0100

    string methods exercise 1

---
+++ javascript/strings/app.js
@@ -0,0 +1,180 @@
+let recenica = "Ana Ana Ana Ana voli Milovana";
+
+// console.log(recenica[recenica.length], recenica.length);
+
+//SLICE
+
+// console.log(recenica.slice(-16, -3));
+
+//SUBSTRING
+
+// console.log(recenica.substring(-16, -3));
+
+//SUBSTR
+
+// console.log(recenica.substr(2, 5));
+
+//REPLACE, REPLACE ALL
+
+// console.log(recenica.replace("Ana", "Amina"));
+// console.log(recenica.replaceAll("Ana", "Amina"));
+
+// console.log(recenica.replace("voli", "mrzi"));
+
+// toLowerCase(), toUpperCase()
+
+// console.log(recenica.toLowerCase());
+// console.log(recenica.toUpperCase());
+
+//CONCAT
+
+// let recenica2 = "Osisao se Amin";
+
+// console.log(recenica2 + recenica);
+// console.log(recenica2.concat(recenica));
+// console.log(recenica.concat(recenica2));
+
+//TRIM, trimEnd, trimStart
+
+// let trimRecenica = "    Tarik je Prof   ";
+
+// console.log(trimRecenica.trim());
+
+// console.log(trimRecenica.trimEnd());
+
+//padStart, padEnd
+
+// let primer = "nes";
+
+// console.log(primer.padStart(5, "0"));
+// console.log(primer.padEnd(4, "0"));
+
+//CharAt, CharCodeAt, []
+
+// let recenica3 = "Hasane ostavi telefon";
+
+// console.log(recenica3[2]);
+// console.log(recenica3.charAt(2));
+// console.log(recenica3.charCodeAt(2));
+
+//SPLIT
+// let niz = []
+// for (let i = 0; i < recenica.length; i++) {
+//     // console.log(recenica[i]);
+//     niz.push(recenica[i])
+// }
+
+// console.log(niz);
+
+// console.log(recenica);
+
+// let niz = recenica.split("")
+
+// console.log(niz);
+
+//JOIN
+
+// console.log(niz.join(""));
+
+//SPLICE
+
+// console.log(recenica.split(" ").splice(0, 3, "NESTO").join());
+
+//indexOf, match, matchAll, includes()
+
+// console.log(recenica.indexOf("Tarik"));
+// console.log(recenica.lastIndexOf("Ana"));
+
+// console.log(recenica.match());
+
+// let text = "The rain in SPAIN stays mainly in the plain";
+// const myArr = text.match("SPAIN");
+
+// let text = "The rain in SPAIN stays mainly in the plain";
+// const myArr = text.matchAll("ain");
+
+// console.log(myArr);
+
+// console.log(recenica.includes("mrzi"));
+
+//Sastaviti program za ispisivanje broja reci koje ce korisnik da unese
+
+// let rec = prompt("Unesite recenicu")
+
+// console.log(rec.split(" ").length);
+
+// 8.6. Саставити програм за одређивање броја самогласника и сугласника у улазном тексту.
+//  Унос текста се завршава сигналом ЕОF.
+
+// let samoglasnici = ["a", "e", "i", "o", "u"];
+// while(true){
+//     let brSamoglasnici = 0;
+//     let suglasnici = 0;
+//     let rec = prompt("Unesite rec: ")
+//     if(rec === "0")
+//         break
+//     for(let i = 0; i < rec.length; i++)
+//     if (samoglasnici.includes(rec[i]))
+//     // if(rec[i] === "a" ||rec[i] === "e" || rec[i] === "i" || rec[i] === "o" || rec[i] === "u")
+//     {
+//         brSamoglasnici++
+//     }
+//     else{
+//         suglasnici++;
+//     }
+
+//     console.log(`Ima ${suglasnici} suglasnika, i ${brSamoglasnici} samoglasnika`);
+// }
+
+// 8.7. Саставити програм за одређивање броја великих слова, малих слова и цифара у улазном
+// тексту. Унос текста се завршава сигналом ЕОF.
+
+// let niz = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+// while (true) {
+//   let velikaSlova = 0;
+//   let malaSlova = 0;
+//   let brojBrojeva = 0;
+//   let rec = prompt("Unesite rec: ");
+//   if (rec === "0") break;
+//   for (let i = 0; i < rec.length; i++) {
+//     if (niz.includes(parseInt(rec[i]))) {
+//       brojBrojeva++;
+//     } else if (rec[i] === rec[i].toUpperCase()) {
+//       velikaSlova++;
+//     } else {
+//       malaSlova++;
+//     }
+//   }
+
+//   console.log(
+//     `Velika slova ${velikaSlova} i mala slova ${malaSlova}, broj cifara je ${brojBrojeva}`
+//   );
+// }
+
+// 8.8. Саставити програм за одређивање броја појављивања слова А у улазном тексту и изразити ту
+// вредност процентуално у односу на све унете знаке. Унос текста се завршава сигналом ЕОF. 
+
+
+while(true){
+    let rec = prompt("Unesite rec: ");
+    let brojA = 0;
+
+    if(rec.includes("A")){
+        // for(let slovo in rec){
+        //     console.log(slovo);
+        //     if(slovo === "A")
+        //     {
+        //         console.log("Proslo");
+        //             brojA++;
+        //     }
+        // }
+        for(let i = 0; i < rec.length; i++){
+            if(rec[i] === "A"){
+                brojA++
+            }
+        }
+    }
+
+    console.log(`Procentalno slova A u reci ima ${brojA/rec.length * 100}%`);
+}
