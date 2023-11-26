de20f9186478fc51df3faf75e333fbec1f10658e
Tarik Ibrahimovic <tarikibrahimovic2016@gmail.com>
2023-11-26 20:12:11 +0100

    array exercise

--- javascript/nizovi/app.js
+++ javascript/nizovi/app.js
@@ -336,11 +336,10 @@
 
 // console.log(`Aritmeticka sredina je: ${res / t}`);
 
-
 // 10.9. Саставити програм који ће за унети низ (дужине n) целих бројева одредити и исписати:
 // а) суму парних и суму непарних бројева;
-// б) суму елемената са парним индексима и суму елемената са непарним индексима. 
- 
+// б) суму елемената са парним индексима и суму елемената са непарним индексима.
+
 // let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
 // let niz = [];
 
@@ -373,11 +372,9 @@
 
 // console.log(`Zbir parnih je: ${resParnih}, a neparnih je: ${resNeparnih}`);
 
-
 // 10.11. Саставити програм који учита елементе низа A A A A = ( 1 2 , ,..., n ) дужине n и низа
 // B B B B = ( 1 2 , ,..., m ) дужине m и спаја у један низ C A A A B B B = ( 1 2 1 2 , ,..., , , ,..., n m ) дужине n+m.
-// Исписати новокреирани низ. 
-
+// Исписати новокреирани низ.
 
 // let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
 // let niz = [];
@@ -399,7 +396,7 @@
 
 // 10.12. Саставити програм који учита елементе два низа низа дужине n A A A A = ( 1 2 , ,..., n ) и
 // B B B B = ( 1 2 , ,..., n ) и формира и исписује нови низ чији су елементи
-// C A B A B A B = + + + ( 1 1 2 2 , ,..., n n ). 
+// C A B A B A B = + + + ( 1 1 2 2 , ,..., n n ).
 
 // let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
 // let niz = [];
@@ -455,7 +452,7 @@
 
 // 10.15. Саставити програм који учита елементе низа A A A A = ( 1 2 , ,..., n ) дужине n и низа
 // B B B B = ( 1 2 , ,..., m ) дужине m, а затим формира и исписује низ С који се састоји од парних елемената
-// низа А и низа В. 
+// низа А и низа В.
 
 // let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
 // let niz = [];
@@ -481,11 +478,10 @@
 
 // console.log(niz3);
 
-
 // 10.16. Саставити програм који ће учитати два низа целих бројева А и В једнаких дужина n и на
 // основу њих формирати низ С тако да i-ти елемент низа С буде једнак мањем од i-тих елемената низа
 // А и В. Ако су i-ти елементи низа А и В једнаки онда i-ти елемент низа С треба да добије вредност
-// нула. Исписати низ С. 
+// нула. Исписати низ С.
 
 // let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
 // let niz = [];
@@ -517,7 +513,7 @@
 // console.log(niz3);
 // 10.17. Саставити програм који за учитани низ целих бројева А дужине n формира и исписује два
 // низа: низ В који садржи негативне елементе низа А и низ С који садржи позитивне елементе и нуле
-// низа А. 
+// низа А.
 
 // let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
 // let niz = [];
@@ -534,9 +530,8 @@
 
 // console.log(`Niz za pozitivne: ${niz2} i niz za negativne je: ${niz3}`);
 
-
 // 10.18. Саставити програм који за унети низ реланих бројева А дужине n формира и исписује нови
-// низ В кога чине елементи низа А који су већи од аритемтичке средине свих елемената низа А. 
+// низ В кога чине елементи низа А који су већи од аритемтичке средине свих елемената низа А.
 
 // let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
 // let niz = [];
@@ -557,7 +552,6 @@
 //     return clan
 //   }
 // })
-
 
 // console.log(niz2);
 
@@ -582,44 +576,40 @@
 
 // console.log(niz2);
 
-
 // 10.22. Саставити функцију којом се одређује број различитих елемената у задатом целобројном
 // низу. Затим саставити програм који чита низ целих бројева, и одређује број различитих елемената
-// користећи претходну функцију и исписује резултат. 
+// користећи претходну функцију и исписује резултат.
 
+// let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
+// let niz = [];
 
-let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
-let niz = [];
+// for (let i = 0; i < brojClanova; i++) {
+//   let broj = parseFloat(prompt("Unesite broj:"));
+//   niz.push(broj);
+// }
 
-for (let i = 0; i < brojClanova; i++) {
-  let broj = parseFloat(prompt("Unesite broj:"));
-  niz.push(broj);
-}
+// function numberOfUnique(niz){
+//   let set = new Set(niz)
+//   console.log(set);
 
-function numberOfUnique(niz){
-  let set = new Set(niz)
-  console.log(set);
+//   return set.size
+// }
 
-  return set.size
-}
+// let pom = niz.filter((value, index, self) => {
+//   return self.indexOf(value) === index
+// });
 
-let pom = niz.filter((value, index, self) => {
-  return self.indexOf(value) === index
-});
-
-console.log(pom);
+// console.log(pom);
 
 // console.log(numberOfUnique(niz));
 
-
 // 10.23. Саставити програм за израчунавање статистике полагања испита која обухвата следеће
 // функције: израчунавање укупне просечне оцене (сви који су полгали испит), израчунавање просечне
-// оцене оних који су положили (оцена већа од 5), 
+// оцене оних који су положили (оцена већа од 5),
 // израчунавање броја који су положили испит,
 // израчунавање броја који нису положили испит и израчунавање броја који имају оцену већу од
 // просечне. У главном програму се уноси број студената и оцене студената у облику низа. Исписати
 // добијене резултате
-
 
 // let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
 // let main = [];
@@ -628,7 +618,6 @@ console.log(pom);
 //   let broj = parseFloat(prompt("Unesite broj:"));
 //   main.push(broj);
 // }
-
 
 // function avgGrade(niz){
 //   let suma = niz.reduce((a,b) => {
@@ -652,7 +641,6 @@ console.log(pom);
 //   }).length
 // }
 
-
 // function numOfFail(niz){
 //   return niz.filter(clan => {
 //     if(clan === 5)
@@ -674,3 +662,158 @@ console.log(pom);
 // console.log(numOfSuccess(main));
 // console.log(numOfFail(main));
 // console.log(numOfAvgSuccess(main));
+
+// 10.24. Саставити функцију која генерише првих n чланова Фибоначијевог низа и функцију која
+// исписује чланове низа, а затим те функције тестирати у главном програму.
+// Фибоначијев низ: f1=1, f2=1, fi=fi-1+fi-2, i=3, 4, 5, ...
+
+// let generateFibonaci = (n) => {
+//   let niz = [1, 1];
+//   for(let i = 2; i < n; i++){
+//     niz.push(niz[i-1] + niz[i-2])
+//   }
+//   return niz
+// }
+
+// function ispisi(niz){
+//   console.log(`Fibonacijev niz je: ${niz}`);
+// }
+
+// let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
+
+// let fibonaciNiz = generateFibonaci(brojClanova);
+
+// ispisi(fibonaciNiz);
+
+// 10.27. Саставити програм који за унети низ реалних бројева, дужине n, исписује елемент највеће
+// вредности, као и његове позиције у низу.
+
+// let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
+// let main = [];
+
+// for (let i = 0; i < brojClanova; i++) {
+//   let broj = parseFloat(prompt("Unesite broj:"));
+//   main.push(broj);
+// }
+
+// jedno resenje:
+// let max = 0;
+// let pozicija = 0;
+// for(let i = 0; i < main.length; i++){
+//   if(main[i] > max){
+//     max = main[i]
+//     pozicija = i
+//   }
+// }
+
+// drugo resenje:
+
+// let max = Math.max(...main);
+
+// let pozicija = main.indexOf(max)
+
+// console.log(main);
+
+// console.log(`Max element je: ${max}, a njegova pozicija je: ${pozicija}`);
+
+// 10.28. Саставити програм који учита низ реалних бројева, дужине n, налази најмањи и највећи члан
+// низа, xmin и xmax, и исписује све елементе низа који су мањи од xmax/2 и већи од xmin*2.
+
+// let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
+// let main = [];
+
+// for (let i = 0; i < brojClanova; i++) {
+//   let broj = parseFloat(prompt("Unesite broj:"));
+//   main.push(broj);
+// }
+
+// let max = Math.max(...main);
+// let min = Math.min(...main);
+
+
+// let nizOnajKojiTreba= main.filter((clan) => {
+//   if(clan < max/2 && clan > min*2){
+//     return clan
+//   }
+// })
+// console.log(main);
+// console.log(nizOnajKojiTreba);
+
+
+// 10.29. Саставити програм који за унети низ целих бројева, дужине n, исписује елемент најмање
+// вредности међу парним бројевима.
+
+// let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
+// let main = [];
+
+// for (let i = 0; i < brojClanova; i++) {
+//   let broj = parseFloat(prompt("Unesite broj:"));
+//   main.push(broj);
+// }
+
+//jedno resenje:
+// main = main.filter((clan) => clan % 2 === 0)
+
+// let min = Math.min(...main)
+
+// console.log(min);
+
+//drugo resenje:
+// console.log(Math.min(...main.filter((clan) => clan % 2 === 0)));
+
+// 10.30. Саставити програм који за унети низ целих бројева, дужине n, проналази и на екрану
+// исписује елементе на парним позицијама и међу њима проналази онај који има максималну
+// вредност. Минимална дужина низа је 2.
+
+// let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
+// let main = [];
+
+// for (let i = 0; i < brojClanova; i++) {
+//   let broj = parseFloat(prompt("Unesite broj:"));
+//   main.push(broj);
+// }
+
+// main.forEach((clan, i) => {
+//   i % 2 === 0 && console.log(clan);
+// })
+
+// console.log(Math.max(...main.filter((clan, i) => i % 2 === 0)));
+
+
+// 10.31. Саставити програм који исписује обавештење да ли учитани низ бројева одговара
+// Фибоначијевом низу. Низ бројева који се уноси мора имати најмање три елемента.
+// Фибоначијев низ: f1=1, f2=1, fi=fi-1+fi-2, i=3, 4, 5, ...
+
+
+let brojClanova = parseInt(prompt("Unesite zeljeni broj clanova: "));
+let main = [];
+
+for (let i = 0; i < brojClanova; i++) {
+  let broj = parseFloat(prompt("Unesite broj:"));
+  main.push(broj);
+}
+
+let fibonacijevNiz = [1, 1];
+
+for (let i = 2; i < brojClanova; i++) {
+  fibonacijevNiz.push(fibonacijevNiz[i - 1] + fibonacijevNiz[i - 2]);
+}
+// prvo resenje:
+// let semafor = true
+
+// main.forEach((clan, index) => {
+//   if(clan !== fibonacijevNiz[index]){
+//     semafor = false
+//   }
+// })
+
+// semafor ? console.log("ovo jeste fibonacijev niz") : console.log("ovo nije fibonacijev niz");
+
+//drugo resenje:
+
+if(fibonacijevNiz.join(',') === main.join(',')){
+  console.log("ovo jeste fibonacijev niz")
+}
+else{
+  console.log("ovo nije fibonacijev niz");
+}
