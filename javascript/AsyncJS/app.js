2cee625f1b255364fad33a42668eb9c9833a8ef7
Tarik Ibrahimovic <tarikibrahimovic2016@gmail.com>
2024-01-20 16:54:56 +0100

    fetching and showing data

--- javascript/AsyncJS/app.js
+++ javascript/AsyncJS/app.js
@@ -20,7 +20,9 @@
 //   setTimeout(() => {
 //     console.log("one");
 //   }, 3000);
+
 //   console.log("two");
+
 //   setTimeout(() => {
 //     console.log("three");
 //   }, 1000);
@@ -28,6 +30,84 @@
 
 // one();
 
-setInterval(() => {
-  console.log("nesto");
-}, 1000);
+// setInterval(() => {
+//   console.log("nesto");
+// }, 1000);
+
+//////////////////////////
+
+// let promise = new Promise(function (resolve, reject) {
+//   // a mock async action using setTimeout
+//   setTimeout(function () {
+//     resolve("Promise resolved");
+//   }, 3000);
+// });
+
+// promise.then(
+//   function (value) {
+//     console.log(value);
+//   },
+//   function (reason) {
+//     console.log(reason);
+//   }
+// );
+
+// https://catfact.ninja/fact
+
+// //Get i Delete
+// {
+//   "header"= {
+//     "Content-Type": "application/json",
+//   }
+// }
+
+// //Post i Put
+
+// {
+//   "header"= {
+//     "Content-Type": "application/json",
+//   }
+//   "body"= {
+//     "fact": "A cat's tail contains nearly 10 percent of all the bones in its body.",
+//     "length": 67
+//   }
+// }
+
+// let data = {};
+
+// fetch("https://catfact.ninja/fact")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((res) => {
+//     console.log(res);
+//     data = res;
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// let div = document.createElement("div");
+// div.innerHTML = data.fact;
+
+// document.body.appendChild(div);
+
+function showCatFacts(data) {
+  let div = document.createElement("div");
+  div.innerHTML = data.fact;
+
+  document.body.appendChild(div);
+}
+
+async function getCatFacts() {
+  try {
+    let data = await fetch("https://catfact.ninja/fact");
+    let res = await data.json();
+    console.log(res);
+    showCatFacts(res);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getCatFacts();
